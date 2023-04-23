b2768589aaff0b5c40998844f795a73199e84e33
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-04-23 16:50:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Axum REST API with Sea-ORM with MySQL
+
+### Features
+
+- Not going to use Migration for first release.
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
+
+    println!("Hello, world!");
+}
